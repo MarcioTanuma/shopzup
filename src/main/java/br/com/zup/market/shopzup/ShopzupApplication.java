7421244bb70bcbdffbb3a28e907af54bb3c0507e
MarcioTanuma <86623585+MarcioTanuma@users.noreply.github.com>
2021-08-12 19:16:03 -0300

    Criando Estrutura de Repositórios para a API

---
+++ src/main/java/br/com/zup/market/shopzup/ShopzupApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.market.shopzup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopzupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopzupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/market/shopzup/ShopzupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.market.shopzup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopzupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
